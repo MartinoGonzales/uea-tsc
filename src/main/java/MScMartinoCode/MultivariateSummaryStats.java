28af86215cfb91777f9cfb9f82c6ab164b7baa7d
Martino Gonzales <fax14yxu@UEA.AC.UK>
2019-06-30 12:31:51 +0100

    Changed determineOutputFormat to make it work when no Instances are present in inputFormat

--- src/main/java/MScMartinoCode/MultivariateSummaryStats.java
+++ src/main/java/MScMartinoCode/MultivariateSummaryStats.java
@@ -38,21 +38,42 @@ public class MultivariateSummaryStats extends SimpleBatchFilter {
         // To check that all attribute are numeric for a multivariate format
         // the only way that I found is to extract a instance and iterate 
         // throught its attribute
-        Instance temp = inputFormat.firstInstance();
-        // Then retrieve the relational attribute, there is only one so return 
-        // the one at 0
-        Instances features = temp.relationalValue(0);
-        // Get the first features an iterate through it to check that all 
-        // attribute are numeric. 
-        temp = features.firstInstance();
-        for (int i = 0; i < temp.numAttributes(); i++) {
-            // Not need to check for the class value since we are looking 
-            // at a single feature an not the whole instance
-            if (!temp.attribute(i).isNumeric())
+//        Instance temp = inputFormat.firstInstance();
+//        // Then retrieve the relational attribute, there is only one so return 
+//        // the one at 0
+//        Instances features = temp.relationalValue(0);
+//        // Get the first features an iterate through it to check that all 
+//        // attribute are numeric. 
+//        temp = features.firstInstance();
+//        for (int i = 0; i < temp.numAttributes(); i++) {
+//            // Not need to check for the class value since we are looking 
+//            // at a single feature an not the whole instance
+//            if (!temp.attribute(i).isNumeric())
+//                throw new Exception("Non Numeric attribute not allowed in SummaryStats");
+//        }
+
+        // Check that all the attributes are real valued
+        // Since it is a multivariate format the first attribute will held the 
+        // relational attribute (index 0) while the class index will be at index 1
+        // First of all check that the class index is at the last index of the instance
+        if (inputFormat.classIndex() != inputFormat.numAttributes()-1)
+            throw new Exception("Class index is not at the end of the Instance");
+        // Then get the relational attribute
+        Attribute relAtr = inputFormat.attribute(0);
+
+        // Iterate through attributes and check they are numeric
+        for (int i = 0; i < relAtr.relation(0).numAttributes(); i++) {
+            if (!relAtr.relation(0).attribute(i).isNumeric())
                 throw new Exception("Non Numeric attribute not allowed in SummaryStats");
         }
         
-        
+//        Attribute atr = inputFormat.attribute(0);
+//        atr.name();
+//        atr.isNumeric();
+//        atr.relation(0);atr.relation(0).numAttributes();
+//        atr.toString();
+//        atr.value(0);
+                
         // Create the ARFF format information 
         // In order to create relational attribute found useful to look at https://waikato.github.io/weka-wiki/creating_arff_file/
         // it seems you specify an instance with some attribute and use it as the relational attribute
@@ -93,23 +114,31 @@ public class MultivariateSummaryStats extends SimpleBatchFilter {
     }
 
     @Override
-    protected Instances process(Instances instances) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    protected Instances process(Instances inst) throws Exception {
+        Instances output = determineOutputFormat(inst);
+        
+        // Iterate through Instances
+        for (int i = 0; i < inst.numInstances(); i++) {
+            // Get the series
+            // Since it is multivariate the element at position 0 will be a
+            double [] s = inst.instance(i).toDoubleArray();
+            System.out.println("");
+            
+            
+        }
+        return null;
+        
     }
     
 	public static void main(String[] args) {
 /**Debug code to test SummaryStats generation: **/
-            System.out.println("DioCane");
 		
             try{
                 Instances data = Utilities.loadData("\\\\ueahome4\\stusci3\\fax14yxu\\data\\Documents\\4th year\\Dissertation\\data\\1000InstPerClass_LCdata\\1000LCdata_multivariate");
                 MultivariateSummaryStats mst = new MultivariateSummaryStats();
-                mst.determineOutputFormat(data);
-//                Instances filter=new SummaryStats().process(test);
-//               SummaryStats m=new SummaryStats();
-//               m.setInputFormat(test);
-//               Instances filter=Filter.useFilter(test,m);
-//               System.out.println(filter);
+                mst.setInputFormat(data);
+                Instances filter=Filter.useFilter(data,mst);
+                System.out.println(filter);
             }
             catch(Exception e){
                System.out.println("Exception thrown ="+e);
